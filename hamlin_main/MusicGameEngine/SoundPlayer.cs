fe0a21340ccca41075f15014e4c5bd421792b6ba
s1473202 <michael.romanov@student.kug.ac.at>
2017-12-05 16:08:40 +0100

    Basic scale playing engine implemented for one octave. Picks random scale on random base key and waits for user to play scale upwards. Prints Hit and Miss Messages. Plays win sound or fail sound.

---
+++ hamlin_main/MusicGameEngine/SoundPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// BasicSoundPlayer, just plays sounds by pressing Keys
+// Only one octave implemented yet
+
+public class SoundPlayer : MonoBehaviour {
+
+	public AudioSource c;
+	public AudioSource c_sh;
+	public AudioSource c1;
+	public AudioSource d;
+	public AudioSource d_sh;
+	public AudioSource e;
+	public AudioSource f;
+	public AudioSource f_sh;
+	public AudioSource g;
+	public AudioSource g_sh;
+	public AudioSource a;
+	public AudioSource a_sh;
+	public AudioSource h;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Y)) {
+			c.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			c_sh.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.Comma)) {
+			c1.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.X)) {
+			d.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			d_sh.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.C)) {
+			e.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.V)) {
+			f.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.G)) {
+			f_sh.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.B)) {
+			g.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.H)) {
+			g_sh.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.M)) {
+			h.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.N)) {
+			a.Play ();
+		}
+		if (Input.GetKeyDown(KeyCode.J)) {
+			a_sh.Play ();
+		}
+	}
+}
