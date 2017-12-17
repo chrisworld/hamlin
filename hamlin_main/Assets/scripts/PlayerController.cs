783e1f2e1799b767c8672998cd7ba6e1cc1a1a61
cattius <easdon@gmx.com>
2017-12-17 17:31:40 +0100

    Added jumping, changed rat lab material to grey so it's easier to tell where the walls are, reduced default mouse and arrow key sensitivity so character is easier to control

--- hamlin_main/Assets/scripts/PlayerController.cs
+++ hamlin_main/Assets/scripts/PlayerController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float walkSpeed = 2;
-    public float runSpeed = 6;
+    public float walkSpeed = 1;
+    public float runSpeed = 3;
     public float turnSmoothTime = 0.2f;
     public float speedSmoothTime = 0.1f;
-    public float gravity = -12f;
+    public float gravity = -12;
+    public float jumpHeight = 0.5f;
+    public float inAirControl = 1;          //controls how much the player can turn while in mid air
+
+    //change these to change run and jump control keys
+    KeyCode runKey = KeyCode.LeftShift;
+    KeyCode jumpKey = KeyCode.Space;
 
     float turnSmoothVelocity;
     float speedSmoothVelocity;
@@ -19,32 +25,33 @@ public class PlayerController : MonoBehaviour {
     Transform cameraT;
     CharacterController controller;
 
-	// Use this for initialization
 	void Start () {
         animator = GetComponentInChildren<Animator>();
         cameraT = Camera.main.transform;
         controller = GetComponent<CharacterController>();
     }
 	
-	// Update is called once per frame
 	void Update () {
 
-        //TODO: why are we using raw?
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDirection = input.normalized;
+
+        if (Input.GetKeyDown(jumpKey))
+        {
+            Jump();
+        }
 
         //calculates rotation for player as arctan(x / y)
         //player facing forwards = 0 deg rotation; facing right = 90 deg rotation, etc 
         if (inputDirection != Vector2.zero)
         {
             float targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
         }
 
-        //running activated by pressing left shift; change this key code to use a different key
-        bool running = Input.GetKey(KeyCode.LeftShift);
+        bool running = Input.GetKey(runKey);
         float targetSpeed = (running ? runSpeed : walkSpeed) * inputDirection.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
 
         velocityY += Time.deltaTime * gravity;
         Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
@@ -58,8 +65,27 @@ public class PlayerController : MonoBehaviour {
 
         //this is how we tell the animation controller which state we're in
         float animationSpeedPercent = (running ? currentSpeed / runSpeed : currentSpeed/walkSpeed * 0.5f);
-        //Debug.Log("setting animator var to: " + animationSpeedPercent);    //don't think this is working properly
-        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+        animator.SetFloat("speedPercent", animationSpeedPercent, GetModifiedSmoothTime(speedSmoothTime), Time.deltaTime);
 
     }
+
+    void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+
+    //not convinced this is really that useful - may delete later
+    float GetModifiedSmoothTime(float smoothTime)
+    {
+        if (controller.isGrounded) return smoothTime;
+        else if (inAirControl == 0) return float.MaxValue;
+        else return (smoothTime / inAirControl);
+
+    }
+
+
 }
