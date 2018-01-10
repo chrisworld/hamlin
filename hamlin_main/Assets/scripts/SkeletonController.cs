26863bd6f9621efb4c4c58a45935b3fb10bb9663
cattius <easdon@gmx.com>
2018-01-10 15:02:37 +0100

    more combat changes

--- hamlin_main/Assets/scripts/SkeletonController.cs
+++ hamlin_main/Assets/scripts/SkeletonController.cs
@@ -5,81 +5,84 @@ using UnityEngine;
 public class SkeletonController : MonoBehaviour
 {
 
-    public int attackDistance = 10;  //walk towards player if player detected and further away from this distance, else attack player
-
-    //these define the space in which the monster can detect the player
-    public int viewDistance = 20;
-    public int viewAngle = 30;
-
     public Transform player;
     static Animator anim;
-    private ScaleListener scaleListener;
-    private bool inCombat = false;          //used in ScaleListener
 
-    // Use this for initialization
+    //used by CombatManager
+    public bool inCombat = false;
+
+    //managed by CombatManager        
+    public bool playerHasWon = false;
+    public int attackDistance;
+    public int viewDistance;
+    public int viewAngle;
+
+
+
     void Start()
     {
         anim = GetComponent<Animator>();
-        scaleListener = GetComponent<ScaleListener>();
+
+        //"not initialised" values, initialised by CombatManager
+        attackDistance = -1;
+        viewDistance = -1;
+        viewAngle = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 direction = player.position - this.transform.position;
-        float angle = Vector3.Angle(direction, this.transform.forward);
-
-        print(Vector3.Distance(player.position, this.transform.position));
-        print(angle);
-
-        if (Vector3.Distance(player.position, this.transform.position) < viewDistance && angle < viewAngle)
+        if(attackDistance != -1 && viewDistance != -1 && viewAngle != -1)
         {
 
-            direction.y = 0;
+            Vector3 direction = player.position - this.transform.position;
+            float angle = Vector3.Angle(direction, this.transform.forward);
 
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.05f);
+            print(Vector3.Distance(player.position, this.transform.position));
+            print(angle);
 
-            anim.SetBool("isIdle", false);
-            if (scaleListener.GetWinState())                                 //player has won, monster runs away
+            if (Vector3.Distance(player.position, this.transform.position) < viewDistance && angle < viewAngle)
             {
 
-                print("win state detected");
-                inCombat = false;
-                scaleListener.ResetWinState();
+                direction.y = 0;
+
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.05f);
+
+                anim.SetBool("isIdle", false);
+                if (playerHasWon)                                       //player has won, monster runs away
+                {
+                    //need a translate here to make it change direction
+                    anim.SetBool("isRunning", true);
+                }
+                if (direction.magnitude > attackDistance)               //detected player but too far away to attack, walk towards
+                {
+                    inCombat = true;
+                    this.transform.Translate(0, 0, 0.1f);
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isAttacking", false);
+
+                }
+                else
+                {                                                        //detected player nearby, attack
+                    inCombat = true;
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isAttacking", true);
+                }
             }
-            if (direction.magnitude > attackDistance)                                   //detected player but too far away to attack, walk towards
+
+            else                                                         //not in combat, idle
             {
-                inCombat = true;
-                this.transform.Translate(0, 0, 0.1f);
-                anim.SetBool("isWalking", true);
+                inCombat = false;
+                //TODO: win state needs to be reset
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isWalking", false);
                 anim.SetBool("isAttacking", false);
 
             }
-            else
-            {                                                        //detected player nearby, attack
-                inCombat = true;
-                anim.SetBool("isWalking", false);
-                anim.SetBool("isAttacking", true);
-            }
-        }
-
-        else                                                                //not in combat, idle
-        {
-            scaleListener.ResetWinState();
-            inCombat = false;
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isAttacking", false);
 
         }
 
-    }
-
-    //Used in ScaleListener
-    public bool GetCombatStatus()
-    {
-        return inCombat;
     }
 
 }
