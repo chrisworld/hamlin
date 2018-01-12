5e0b2d2268d22650b5f079153e458b0117183520
cattius <easdon@gmx.com>
2018-01-12 12:17:11 +0100

    added pablo's work to repo - menu assets. not used in game yet

---
+++ hamlin_main/Assets/scripts/menus/graphics.js
@@ -0,0 +1,29 @@
+public function Graphics1(){
+	
+	QualitySettings.SetQualityLevel(0);
+}
+
+public function Graphics2(){
+	
+	QualitySettings.SetQualityLevel(1);
+}
+
+public function Graphics3(){
+	
+	QualitySettings.SetQualityLevel(2);
+}
+
+public function Graphics4(){
+	
+	QualitySettings.SetQualityLevel(3);
+}
+
+public function Graphics5(){
+	
+	QualitySettings.SetQualityLevel(4);
+}
+
+public function Graphics6(){
+	
+	QualitySettings.SetQualityLevel(5);
+}
---
+++ hamlin_main/Assets/scripts/menus/savingOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class savingOptions : MonoBehaviour {
+	public Transform Player;
+
+	public void Load(string i){
+		
+		//SceneManager.LoadScene("GameScene");
+		Player.position = new Vector3(PlayerPrefs.GetFloat("x"), PlayerPrefs.GetFloat("y"), PlayerPrefs.GetFloat("z"));
+		Player.eulerAngles = new Vector3(0, PlayerPrefs.GetFloat("Cam_y"), 0);
+	}
+	
+	public void SaveExit(bool Quit){
+		
+		PlayerPrefs.SetFloat("x", Player.position.x);
+		PlayerPrefs.SetFloat("y", Player.position.y);
+		PlayerPrefs.SetFloat("z", Player.position.z);
+		PlayerPrefs.SetFloat("Cam_y", Player.eulerAngles.y);
+		if(Quit){
+			Time.timeScale = 1;
+			SceneManager.LoadScene("MainMenu");
+		}
+	}
+}
