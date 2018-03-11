b42157adc898a37e5b8425f9499c70d676d194f2
Pablo Borreguero Llorente <borreguerollorente@student.tugraz.at>
2018-03-11 18:46:07 +0100

    changes

---
+++ hamlin_main/Assets/scripts/menus/bgMusicMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgMusicMainMenu : MonoBehaviour {
+
+	public Transform MainMenu;
+	private AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+
+		Time.timeScale = 1;
+		music = MainMenu.gameObject.GetComponent<AudioSource>();
+		music.Play();
+	}
+
+	public void playMainSound(){
+
+		Time.timeScale = 1;
+		music = MainMenu.gameObject.GetComponent<AudioSource>();
+		music.Play();
+	}
+
+}
---
+++ hamlin_main/Assets/scripts/menus/textures.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textures : MonoBehaviour {
+	public void  Graphics1 (){
+	
+		QualitySettings.SetQualityLevel(0);
+	}
+
+	public void  Graphics2 (){
+	
+		QualitySettings.SetQualityLevel(1);
+	}
+
+	public void  Graphics3 (){
+	
+		QualitySettings.SetQualityLevel(2);
+	}
+
+	public void  Graphics4 (){
+	
+		QualitySettings.SetQualityLevel(3);
+	}
+
+	public void  Graphics5 (){
+	
+		QualitySettings.SetQualityLevel(4);
+	}
+
+	public void  Graphics6 (){
+	
+		QualitySettings.SetQualityLevel(5);
+	}
+}
